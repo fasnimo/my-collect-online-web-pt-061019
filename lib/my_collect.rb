22968d7e5006cdffc3ae31f0792e362ab0d7795e
fasnimo <fasnimo@gmail.com>
2020-05-25 11:45:38 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    index = 0
+    collection = []
+    while index < array.length
+        collection << yield(array[index].split(" ").first.capitalize)
+        index += 1
+    end 
+    collection
+end 
 
